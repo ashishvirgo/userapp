5582289d736e9c1e85c93457d4ef716c3d1b2a17
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-15 12:13:48 +0530

    create backend code for user app

---
+++ Server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose =require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+const url=process.env.MONGO_URL;
+
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(url);
+        console.log("MONGODB Connected")
+    }
+    catch(err){
+        console.log("DB Error ",err.message)
+    }
+}
+
+module.exports=connectDB;
