cf635b3ba793abbd9f5ce9df9061bf32a934e9e9
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-16 10:53:01 +0530

    updated user controller and route delete

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -5,5 +5,5 @@ router.get("/users",usercontroller.getUsers);
 router.get("/user/:email",usercontroller.getUser);
 router.put("/edituser/:email",usercontroller.editUser);
 router.post("/createuser",usercontroller.createUser);
-router.post("/deleteuser",usercontroller.deleteUser);
+router.delete("/deleteuser/:email",usercontroller.deleteUser);
 module.exports=router;
