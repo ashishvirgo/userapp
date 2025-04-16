40f9997f726badce0e9145c2dd014753eb8c6da9
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-16 11:05:03 +0530

    updated user model

--- Server/model/userModel.js
+++ Server/model/userModel.js
@@ -4,7 +4,7 @@ const userSchema=new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     role:{type:String,required:true}
-});
+},{timestamps});
 
 const User=mongoose.model("users",userSchema);
 module.exports=User;
