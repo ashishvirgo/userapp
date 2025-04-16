9e3b8148d92db973eb8559fb748d613940540921
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-16 09:58:53 +0530

    updated controller and route

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -3,4 +3,6 @@ const router=express.Router();
 const usercontroller=require("../controller/userController")
 router.get("/users",usercontroller.getUsers);
 router.get("/user/:email",usercontroller.getUser);
+router.put("/edituser/:email",usercontroller.editUser);
+router.post("/createuser/:email",usercontroller.createUser);
 module.exports=router;
--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -18,4 +18,27 @@ const getUser=async(req,res)=>{
         res.status(500).json({message: err.message})
     }
 }
-module.exports={getUsers,getUser}
+const createUser=async(req,res)=>{
+    try{
+        const {name,email,password,role}=req.body;
+        const newUser=new User({name,email,password,role});
+     const user=await newUser.save();
+     res.status(201).json(user);
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
+const editUser=async(req,res)=>{
+    try{
+        const email=req.params.email;
+        const {name,password,role}=req.body;
+    
+     const updateduser=await User.findOneAndUpdate({email},{name,password,role},{new:true});
+     res.status(200).json(updateduser);
+    }
+    catch(err){
+        res.status(500).json({message: err.message})
+    }
+}
+module.exports={getUsers,getUser,createUser,editUser}
